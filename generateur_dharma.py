b0e95f3d25681c84ff3d7600f0bf4e53e9ab0ea0
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2023-05-02 20:36:20 +0200

    ajout d'un système de sauvegarde de ce qui a été généré

--- generateur_dharma.py
+++ generateur_dharma.py
@@ -7,9 +7,20 @@ def create_command(nb = 10, file_name = "ftp_grammar.dg"):
     commande = commande_base + " " + file_name + " -count " + str(nb)
     return commande
 
+def generate_dharma(nb = 10, file_name = "ftp_grammar.dg"):
+    p = subprocess.Popen(create_command(nb = nb, file_name = file_name), shell=True, stdout=PIPE, stderr=PIPE)
+    out, err = p.communicate()
+    list_command = out.decode("utf-8").replace('\r', '').split("\n\n")
+    list_command.pop()
+    return list_command
 
+def write_file(string, file_name = "resultat_dharma.txt", mode = 'w'):
+    f = open(file_name, mode)
+    f.write(string)
+    
 
-p = subprocess.Popen(create_command(), shell=True, stdout=PIPE, stderr=PIPE)
-out, err = p.communicate()
-print(out.decode("utf-8"))
-
+list_command = generate_dharma()
+string = "Liste des commandes générées :\n"
+for c in list_command:
+    string += "- " + c + "\n"
+write_file(string)
