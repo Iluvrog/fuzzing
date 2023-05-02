79fdb1804da1c6a2e0e866dfba0a5f7d82ac6479
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2023-05-02 19:18:20 +0200

    fonction de création de la commande

--- fuzzing.py
+++ fuzzing.py
@@ -1,12 +1,15 @@
 import subprocess
 from subprocess import PIPE
 
-nb = 10
-commande_base = "dharma -grammars"
-file_name = "ftp_grammar.dg"
-command = commande_base + " " + file_name + " -count " + str(nb)
 
-p = subprocess.Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
+def create_command(nb = 10, file_name = "ftp_grammar.dg"):
+    commande_base = "dharma -grammars"
+    commande = commande_base + " " + file_name + " -count " + str(nb)
+    return commande
+
+
+
+p = subprocess.Popen(create_command(), shell=True, stdout=PIPE, stderr=PIPE)
 out, err = p.communicate()
 print(out.decode("utf-8"))
 
