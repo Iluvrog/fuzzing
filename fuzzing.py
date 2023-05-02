956b6a6b4621f0c7f5dd95a315806464fff59aa0
Iluvrog <geoffrey.lacotte.autre@caramail.fr>
2023-05-02 19:08:41 +0200

    plein de génération

--- fuzzing.py
+++ fuzzing.py
@@ -1,9 +1,12 @@
 import subprocess
 from subprocess import PIPE
 
+nb = 10
+commande_base = "dharma -grammars"
+file_name = "ftp_grammar.dg"
+command = commande_base + " " + file_name + " -count " + str(nb)
 
-
-p = subprocess.Popen("dharma -grammars ftp_grammar.dg", shell=True, stdout=PIPE, stderr=PIPE)
+p = subprocess.Popen(command, shell=True, stdout=PIPE, stderr=PIPE)
 out, err = p.communicate()
 print(out.decode("utf-8"))
 
