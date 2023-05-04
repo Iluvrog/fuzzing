63b5b7f5a9a70c9abfe8c83e1200c4a35bed89ab
MattSpeculoos <103248423+MattSpeculoos@users.noreply.github.com>
2023-05-04 18:31:58 +0200

    Update main.py
    Ajout de la connexion socket au serveur FTP

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import src.generateur_dharma as gd
-
+from ftplib import FTP
 
 
 def main():
+    # Génération des commandes 
     list_command = gd.generate_dharma()
     string = "Liste des commandes générées :\n"
     for c in list_command:
         string += "- " + c + "\n"
     gd.write_file(string)
+    
+    # connection au serveur FTP local
+    host = 'localhost'
+    port =  21
+    ftp = FTP((host,port)) 
+       
+    # envoie des commandes
+    for com in list_command:
+            try:
+                res = ftp.sendcmd(com)
+            except:
+                # if error -> save to file ?
+                write_file(com)
+  
     
 if __name__ ==  "__main__":
     main()
